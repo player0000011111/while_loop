481b3fa0040df617f8c99d8758ab56df5562036b
player0000011111 <workingwithgithub@gmail.com>
2023-06-24 18:40:21 +0500

    third commit

---
+++ masala.py
@@ -0,0 +1,36 @@
+# 1. vazifa
+# list1 = [12,13,54,98,12,44,343,23,443,66,23,65,445,65,45]
+
+# pustoy_list = []
+# for i in list1:
+#     pustoy_list.append(i * 2)
+# print(pustoy_list)
+
+# 2.vazifa
+#lst2 = [1,2,33,-114,-13,55,9,-7]
+#s1=0
+#s2=0
+#for i in lst2:
+#    if i>0:
+#        s1 = s1 + i
+#    elif i<0:
+#        s2 = s2 + i
+#print(s1)
+#print(s2)
+
+# 3.vazifa
+# lst2 = [1,2,33,-114,-13,55,9,-7]
+# a = 0
+# for i in lst2:
+#     if i>0 and i%2 !=0:
+#         a = a+i
+#         print(a)
+
+# 5.vazifa
+s = 0
+for i in range(-50,50):
+    if i < 0:
+        s = s+i
+        print(s)
+    else:
+        print(0)
---
+++ task.py
@@ -0,0 +1,43 @@
+# While loop
+a = 1
+
+while a<10:
+    print("salom")
+    a = a + 1  # sanagich
+
+
+# while bilan else
+i = 1
+while i < 6:
+    print(i)
+    i = i + 1
+else: 
+    print("i soni 6 dan katta emas")
+
+# while bilan break
+x = 3
+
+while x < 12:
+    print(x)
+    x = x + 1
+    if x == 9:
+        break
+
+# while bilan continue
+x = 5
+
+while x < 9:
+    print(x)
+    x = x + 1
+    if x == 7:
+        continue
+
+
+
+
+# while bilan pass
+a = 5 
+while a<10:
+    print("Salom")
+
+# 
